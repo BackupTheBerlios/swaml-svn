4f3d3ad293332be5adb25ed8508b7310abd1c199
wikier <wikier@bf4b227d-bf01-0410-9a9f-f6af0c077b00>
2005-10-12 09:00:15 +0000

    little changes

--- webpage/index.php
+++ webpage/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
  <head>
   <title>SWAML - Semantic Web Archive of Mailing Lists</title>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
@@ -35,42 +35,29 @@
   <!-- toolbar -->
   <div class="toolbar">
    <div id="navbuttons">
-    <table width="100%" cellpadding="0" cellspacing="0" border="0">
-     <tr valign="middle">
-      <td align="left">
-       <a href="/" class="wiki">Home</a>
-       &nbsp;|&nbsp;
-       <a href="#news" class="wiki">News</a>
-       &nbsp;|&nbsp;
-       <a href="#doc" class="wiki">Documentation</a>
-       &nbsp;|&nbsp;
-       <a href="#files" class="download">Files</a>
-       &nbsp;|&nbsp;
-       <a href="/wiki" class="wiki">Wiki</a>
-       &nbsp;|&nbsp;
-       <a href="#contact" class="wiki">Contact</a>
-      </td>
-      <td align="right" width="150">
-       <div id="search">
-        <form action="http://www.google.es/search" method="get">
-         <nobr>
-	  <img src="/images/search.png" class="wiki-button" alt="search" width="22" height="22"/>
-	  <input type="text" name="q" value="" />
-          <input name="sitesearch" value="swaml.berlios.de" type="hidden"/>
-	 </nobr>
-        </form>
-       </div>
-      </td>
-     </tr>
-    </table>
+    <a href="/" class="wiki">Home</a> &nbsp;|&nbsp;
+    <a href="#news" class="wiki">News</a> &nbsp;|&nbsp;
+    <a href="#doc" class="wiki">Documentation</a> &nbsp;|&nbsp;
+    <a href="#files" class="download">Files</a> &nbsp;|&nbsp;
+    <a href="/wiki" class="wiki">Wiki</a> &nbsp;|&nbsp;
+    <a href="#contact" class="wiki">Contact</a>
+   </div>
+   <div id="search">
+    <form action="http://www.google.es/search" method="get">
+     <img src="/images/search.png" class="wiki-button" alt="search" width="22" height="22" />
+     <input type="text" name="q" value="" />
+     <input name="sitesearch" value="swaml.berlios.de" type="hidden" />
+    </form>
    </div>
   </div>
+
+  <br/><br/><br/>
 
   <!-- Page content -->
   <div id="content">
    <p>
      <acronym title="Semantic Web Archive of Mailing Lists">SWAML</acronym> is  a research 
-     project around the semantic web tecnologies to publish the mailing lists's archive into 
+     project around the semantic web tecnologies to publish the mailing lists&acute;s archive into 
      an <acronym title="Resource Description Framework">RDF</acronym> format, developed 
      at <a href="http://www.euitio.uniovi.es/">University of Oviedo</a> (Spain). You can
      visit the <a href="http://developer.berlios.de/projects/swaml/">project page at BerliOS</a>
@@ -79,8 +66,7 @@
 
    <h2>
     <a href="#news"></a>News &nbsp; 
-    <a href="http://developer.berlios.de/export/rss20_bsnews.php?group_id=4806" type="application/rss+xml"><img 
-    src="images/xml.png" width="36" height="14" alt="XML"/></a>
+    <a href="/rss/news" type="application/rss+xml"><img src="images/xml.png" width="36" height="14" alt="XML"/></a>
    </h2>
    <div class="wikitext">
     <?php
@@ -108,20 +94,20 @@
 
 	    $num_items = 10;
 	    $items = array_slice($rss->items, 0, $num_items);
-	    echo "<ul>\n";
+	    echo "<dl>\n";
 
 	    foreach ($items as $item) {
-	      $news++;
 	      $title = $item[title];
 	      $link  = $item[link];
 	      $description  = $item[description];
 	      $pubDate  = $item[pubdate];
 	      $date = explode(" ", $pubDate);
-	      echo "      <li>[" . $date[1] . "-" . $months[$date[2]] . "-" . $date[3] . "]\n";
-              echo " <a href=\"" . $link . "\">" . $title . "</a>: " . $description . "</li>\n";
+	      echo "      <dt>[" . $date[1] . "-" . $months[$date[2]] . "-" . $date[3] . "]";
+              echo " <a href=\"" . $link . "\">" . $title . "</a></dt>\n";
+	      echo "      <dd>" . $description . "</dd>\n";
 	    }
 
-	    echo "    </ul>\n\n";
+	    echo "    </dl>\n\n";
 
           } else { echo "Error: " . $rss->ERROR; }
 
@@ -130,29 +116,42 @@
 
    <h2><a href="#doc"></a>Documentation</h2>
    <div class="wikitext">
-    There aren't documentation until the moment.
+    <dl>
+     <dt><a href="http://download.berlios.de/swaml/propuesta.pdf">propuesta.pdf</a>:</dt>
+     <dd>a document in Spanish with the initial idea that reason the development of this project</dd>
+    </dl>  
    </div>
 
    <h2><a href="#files"></a>Files</h2>
    <div class="wikitext">
-    There aren't files until the moment. While you can visit the 
-    <a href="http://svn.berlios.de/wsvn/swaml">subversion repository web interface</a>.
+    <p>There aren not files until the moment. While you can visit the <a href="/files">files section</a> 
+    or <a href="/wsvn">subversion repository web interface</a>.</p>
    </div>
 
    <h2><a href="#contact"></a>Contact</h2>
    <div class="wikitext">
+    <p>You can contact to any <a href="/members-list"> members of project</a>. Also you have a several 
+    <a href="/lists">mailing lists</a>:</p>
     <ul>
-     <li>Sergio Fdez &lt;<a href="mailto:wikierATasturlinux.org">wikierATasturlinux.org</a>&gt;</li>
-     <li>Diego Berrueta &lt;<a href="mailto:diegoATberrueta.net">diegoATberrueta.net</a>&gt;</li>
-     <li>Jose E. Labra &lt;<a href="mailto:labraATuniovi.es">labraATuniovi.es</a>&gt;</li>
+     <li><a href="/lists/devel">Developers mailing list</a></li>
+     <li><a href="/lists/users">Users mailing list</a></li>
+     <li><a href="/lists/spanish">Spanish Users mailing list</a></li>
     </ul>
+    <br/><p>Also, you can <a href="/bugs">report a bug</a> or sending a <a href="/features">feature request</a> 
+    without you need to subscribe of any of <a href="/lists">project mailing lists</a>.</p>
    </div>
 
    <br/><br/><br/>
 
    <div id="revision">
-    <p class="editdate">Last update on Mon 3 October 2005</p>
+    <p class="editdate">Last update on Wed 12 October 2005</p>
    </div>
+  </div>
+
+  <div id="footer">
+     <a href="/doap.rdf" type="application/rdf+xml"><img src="images/rdfmeta.png" alt="RDF Meta" /></a> &nbsp; 
+     <a href="http://validator.w3.org/check/referer"><img src="images/xhtml.png" alt="Valid XHTML" /></a> &nbsp;
+     <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="images/css.png" alt="Valid CSS" /></a> &nbsp;
   </div>
 
  </body>
