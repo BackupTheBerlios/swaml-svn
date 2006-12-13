5dab1f6f898a2e0859cc7b0f4682ee1f5bdaf70e
wikier <wikier@bf4b227d-bf01-0410-9a9f-f6af0c077b00>
2006-12-13 22:19:38 +0000

    deleted pygtk dependecy on swaml package

--- branches/swaml-deb/classes/ui.py
+++ branches/swaml-deb/classes/ui.py
@@ -19,9 +19,6 @@
 """Common functions for UIs"""
 
 import sys, os, string
-import pygtk
-pygtk.require('2.0')
-import gtk
 
 class UI:
     """
@@ -78,81 +75,3 @@ class CommandLineUI(UI):
                 
         UI.__init__(self, id, base+'includes/ui/line/')
 
-    
-class GtkUI(UI):
-    """
-    Abstract class for GTK User Interfaces
-    """    
-    
-    def usage(self):
-        """
-        Print usage information
-        """
-                
-        path = self.lineBase + 'usage/' + self.id + '.txt'
-        
-        try:
-            for line in open(path):
-                print line,
-        except IOError, details:
-                print 'Problem reading from ' + path + ': ' + str(details)
-                
-        sys.exit()
-    
-    def alert(self, text):
-        """
-        Alert window
-        
-        @param text: text on alert
-        """
-        
-        self.alertWindow = gtk.Window(gtk.WINDOW_POPUP)
-        self.alertWindow.set_position(gtk.WIN_POS_CENTER_ALWAYS)
-        self.alertWindow.set_modal(True)
-        self.alertWindow.set_resizable(False)
-        self.alertWindow.set_border_width(0)
-        
-        vbox = gtk.VBox(False, 5)
-        vbox.set_border_width(10)
-        self.alertWindow.add(vbox)
-        vbox.show()
-                
-        align1 = gtk.Alignment(0.5, 0.5, 0, 0)
-        vbox.pack_start(align1, False, False, 5)
-        align1.show()
-        label = gtk.Label(text)
-        align1.add(label)
-        label.show()
-        
-        align2 = gtk.Alignment(0.5, 0.5, 0, 0)
-        vbox.pack_start(align2, False, False, 5)
-        align2.show()        
-        button = gtk.Button('OK')
-        button.connect('clicked', self.destroyAlert, 'cool button')
-        align2.add(button)
-        button.show()
-        
-        self.alertWindow.show()
-        
-    def destroyAlert(self, widget=None, other=None):
-        """
-        Destroy aler window
-        
-        @param widget: widget
-        @param other: other
-        """
-        
-        self.alertWindow.destroy() 
-    
-    def __init__(self, id=None, base='./'):
-        """
-        Constructor method
-        
-        @param id: string id
-        @param base: base directory
-        """
-                
-        UI.__init__(self, id, base)
-        self.lineBase = self.base + 'includes/ui/line/'
-        self.graphicalBase = self.base + 'includes/ui/graphical/'
-    
